e688ff53756a290b99a2743f0dca39811cef199c
alexander m. innerbichler <alexinnerbichler43@gmail.com>
2024-11-13 20:07:00 +0100

    some more stuff

--- main.go
+++ main.go
@@ -28,11 +28,16 @@ func main() {
 	log.Println("got all for " + userId + " successfully")
 
 	value := 0.0
+	issuePrice := 0.0
 	for _, pos := range positions {
 		value += pos.CurrentPrice
+		issuePrice += pos.IssuePrice
 	}
-	value = math.Floor(value)
+	gain := ((value / issuePrice) - 1) * 100
+	value = math.Floor(value*100) / 100
+	gain = math.Floor(gain*100) / 100
 	log.Println("current value of portfolio:", value, "€")
+	log.Println("with a current gain of", gain, "%")
 	time.Sleep(10 * time.Second)
 	log.Println("webWorker completed")
 }
