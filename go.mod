df3132cea68ecd1212d6fa2d3898bdbddf5ed2db
alex wsl pc <alexinnerbichler43@gmail.com>
2024-11-22 00:15:43 +0100

    working scraper with cmdline args

--- go.mod
+++ go.mod
@@ -1,19 +1,20 @@
 module github.com/innerbichler/flatech
 
-go 1.18
+go 1.21.1
 
 require (
-	github.com/glebarez/go-sqlite v1.22.0
+	github.com/innerbichler/flatech/webWorker v0.0.0-20241121224430-9756c4850fdc
 	github.com/joho/godotenv v1.5.1
-	github.com/tebeka/selenium v0.9.9
 )
 
 require (
 	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/glebarez/go-sqlite v1.22.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/tebeka/selenium v0.9.9 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	modernc.org/libc v1.37.6 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
---
+++ scraper/scraper.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/innerbichler/flatech/webWorker"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	filePtr := flag.String("file", "../.env", "path to your .env file, that holds USERID and PASSWORD")
+	dbPtr := flag.String("database", "./test", "path to your database file")
+	minutePtr := flag.Int("time", 10, "time between scrapes in minutes")
+
+	flag.Parse()
+
+	log.Println("+++++++++++++++++++++++++++++++")
+	log.Println("Starting scraper for flatech with options:")
+	log.Println("file", *filePtr)
+	log.Println("database", *dbPtr)
+	log.Println("time", *minutePtr, "min")
+	log.Println("+++++++++++++++++++++++++++++++")
+
+	err := godotenv.Load(*filePtr)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	userId := os.Getenv("USERID")
+	password := os.Getenv("PASSWORD")
+
+	conn, err := webWorker.GetDatabaseConnection(*dbPtr)
+	if err != nil {
+		log.Fatal("Error connecting to db")
+	}
+
+	conn.Startup()
+	defer conn.Connection.Close()
+
+	for {
+		portfolio := scrapePortfolio(userId, password)
+		conn, err := webWorker.GetDatabaseConnection(*dbPtr)
+		if err != nil {
+			log.Fatal("Error connecting to db")
+		}
+
+		conn.Startup()
+		conn.InsertPortfolio(portfolio)
+		log.Println("scraped portfolio for ******** successfully")
+		time.Sleep(time.Duration(*minutePtr) * time.Minute)
+	}
+}
+
+func scrapePortfolio(userId string, password string) webWorker.Portfolio {
+	worker := webWorker.NewWebWorker(userId, password)
+	defer worker.Close()
+	worker.Login()
+	portfolio := worker.GetPortfolio()
+	return portfolio
+}
