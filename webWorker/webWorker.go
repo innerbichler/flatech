14aea7a29e6d19ba575bd746aa2d3fffa326de23
alex wsl pc <alexinnerbichler43@gmail.com>
2024-11-22 01:22:42 +0100

    test

--- webWorker/webWorker.go
+++ webWorker/webWorker.go
@@ -22,6 +22,10 @@ type WebWorker struct {
 	service  *selenium.Service
 }
 
+func SWAG() {
+	log.Println("swag")
+}
+
 func NewWebWorker(userId string, password string) WebWorker {
 	// starts webdriver and logs in -> returns the driver instance
 	opts := []selenium.ServiceOption{
